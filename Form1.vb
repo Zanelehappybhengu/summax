e81870bcb3995362474edc57cfa3baf41ce53d1d
Dell-User <Dell-User@DESKTOP-S7HKJ5C>
2024-01-17 16:11:25 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim first, second As Double
+
+        first = Val(TextBox1.Text)
+        second = Val(TextBox2.Text)
+
+        If first > second Then
+            TextBox3.Text = first
+        Else
+            TextBox3.Text = second
+        End If
+    End Sub
+End Class
